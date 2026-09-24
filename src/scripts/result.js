b9693628af59eb6303e67d7bc9165775021ad24b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2026-09-24 19:28:28 +0000

    fix: adjust display text in quiz results

--- src/scripts/result.js
+++ src/scripts/result.js
@@ -2,6 +2,8 @@ const correctAnswersElement = document.getElementById("correct-answers")
 const totalQuestionsElement = document.getElementById("total-questions")
 const percentageElement = document.getElementById("percentage")
 
+const params = new URLSearchParams(window.location.search)
+
 const correctAnswers = Number(params.get("correct"))
 const totalQuestions = Number(params.get("total"))
 
