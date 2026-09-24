167fb658b0fc0c2cc45f6a0aaf695a31de6f1f2e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2026-09-24 15:05:02 +0000

    feat: implement dynamic question generation logic

--- src/scripts/questions.js
+++ src/scripts/questions.js
@@ -16,12 +16,54 @@ export const notes = [
     { name: "Si", letter: "B" }
 ]
 
-export function generateQuestions() {
-    return [
-        generateNoteToLetterQuestion(notes[0], notes),
-        generateLetterToNoteQuestion(notes[3], notes),
-        generateNextNoteQuestion(notes[2], notes),
-        generatePreviousNoteQuestion(notes[5], notes),
-        generateSequenceQuestion(notes)
-    ]
+const questionGenerators = [
+    (notes) => generateNoteToLetterQuestion(
+        getRandomNote(notes),
+        notes
+    ),
+
+    (notes) => generateLetterToNoteQuestion(
+        getRandomNote(notes),
+        notes
+    ),
+
+    (notes) => generateNextNoteQuestion(
+        getRandomNote(notes),
+        notes
+    ),
+
+    (notes) => generatePreviousNoteQuestion(
+        getRandomNote(notes),
+        notes
+    ),
+
+    (notes) => generateSequenceQuestion(notes)
+]
+
+function getRandomNote(notes) {
+    const randomIndex = Math.floor(
+        Math.random() * notes.length
+    )
+
+    return notes[randomIndex]
+}
+
+function getRandomGenerator() {
+    const randomIndex = Math.floor(
+        Math.random() * questionGenerators.length
+    )
+
+    return questionGenerators[randomIndex]
+}
+
+export function generateQuestions(amount = 10) {
+    const questions = []
+
+    for (let i = 0; i < amount; i++) {
+        const generateQuestion = getRandomGenerator()
+
+        questions.push(generateQuestion(notes))
+    }
+
+    return questions
 }
